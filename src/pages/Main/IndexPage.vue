e36b088356cac48e82d146bae9352ff37ea41466
mihkek <mihkek991@gmail.com>
2023-02-17 21:18:44 +0300

    made help with house block

--- src/pages/Main/IndexPage.vue
+++ src/pages/Main/IndexPage.vue
@@ -48,12 +48,42 @@
         </div>
       </section>
     </div>
+    <div class="container m-auto my-10 q-px-sm">
+      <q-card
+        class="w-full h-48 md:h-80 lg:h-96 help-section bg-no-repeat bg-contain bg-right-bottom rounded-2xl"
+      >
+        <div
+          class="flex flex-col h-full justify-between align-start p-5 md:p-7"
+        >
+          <h3>
+            Нужна помощь <br />
+            с подбором <br />
+            жилья?
+          </h3>
+          <q-btn
+            rounded
+            color="black"
+            text-color="white"
+            class="w-1/3 py-1 md:py-2 xl:py-4"
+          >
+            <p
+              class="tracking-wider text-t5 md:text-t3 lg:text-t4 q-pa-none whitespace-nowrap"
+            >
+              Оставить заявку
+            </p>
+          </q-btn>
+        </div>
+      </q-card>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .banner-section {
-  background-image: url("/images/main-banner-bg.png");
+  background-image: url("/images/index-page/main-banner-bg.png");
+}
+.help-section {
+  background-image: url("/images/index-page/help-bg.png");
 }
 </style>
 <script>
