b29ecfe953e0eb34cd3be87051da5c3ae5659b5b
mihkek <mihkek991@gmail.com>
2023-02-19 12:55:16 +0300

    made fot hosts and load-app sections

--- src/pages/Main/IndexPage.vue
+++ src/pages/Main/IndexPage.vue
@@ -44,7 +44,7 @@
       </div>
     </section>
 
-    <div class="container m-auto">
+    <section class="container m-auto">
       <section class="text-gray-600 body-font bg-gray-100">
         <div class="container py-24 mx-auto">
           <HelloWorld />
@@ -52,8 +52,8 @@
           <!--          <PopularComponent />-->
         </div>
       </section>
-    </div>
-    <div class="container m-auto mt-10 mb-5 pl-2">
+    </section>
+    <section class="container m-auto mt-10 mb-5 pl-2">
       <h2 class="mb-3">Популярные направления</h2>
       <div
         class="flex flex-row flex-nowrap overflow-x-auto gap-x-2 w-screen pr-3 py-5"
@@ -65,8 +65,8 @@
           :title="item.title"
         />
       </div>
-    </div>
-    <div class="container m-auto my-10 q-px-sm">
+    </section>
+    <section class="container m-auto my-10 q-px-sm">
       <q-card
         class="w-full h-48 md:h-80 lg:h-96 help-section bg-no-repeat bg-contain bg-right-bottom rounded-2xl"
       >
@@ -85,14 +85,80 @@
             class="w-1/3 py-1 md:py-2 xl:py-4"
           >
             <p
-              class="tracking-wider text-t4 md:text-h5 lg:text-h4 q-pa-none whitespace-nowrap"
+              class="tracking-wider text-t4 md:text-h5 lg:text-h4 q-pa-none font-semibold"
             >
               Оставить заявку
             </p>
           </q-btn>
         </div>
       </q-card>
-    </div>
+    </section>
+    <section
+      class="hosts-section full-width h-screen/3 md:h-screen/2 lg:h-screen*90 bg-no-repeat bg-cover bg-center"
+    >
+      <div
+        class="container m-auto flex flex-col justify-between items-start full-height py-7 px-5 sm:px-0"
+      >
+        <div class="flex flex-col items-start">
+          <r-logo
+            class="mb-3"
+            text_size_class="text-h5"
+            text_color_class="text-white"
+            :always_text="true"
+          />
+          <h3 class="text-white">
+            Сдавайте жильё <br />
+            в аренду
+          </h3>
+        </div>
+        <q-btn
+          rounded
+          color="white"
+          text-color="black"
+          no-caps
+          class="w-2/5 py-1 md:py-2 xl:py-4"
+        >
+          <p
+            class="tracking-wider text-t3 md:text-h5 lg:text-h4 q-px-sm whitespace-nowrap font-semibold"
+          >
+            Узнать подробнее
+          </p>
+        </q-btn>
+      </div>
+    </section>
+
+    <section
+      class="load-app-section full-width h-screen/3 md:h-screen/2 lg:h-screen*90 bg-no-repeat bg-cover bg-center mb-5"
+    >
+      <div
+        class="container m-auto flex flex-col justify-between items-start full-height py-7 px-5 sm:px-0"
+      >
+        <div class="flex flex-col items-start">
+          <h3 class="text-white mb-3">
+            Скачайте бесплатное <br />
+            приложение
+          </h3>
+          <p class="font-light text-t3 text-white">
+            Выбирайте маршруты, путешествуйте, <br />
+            с комфортом бронируйте жилье
+          </p>
+        </div>
+        <div class="flex flex-row items-center justify-between"></div>
+        <q-btn
+          rounded
+          color="white"
+          text-color="black"
+          no-caps
+          class="w-2/5 py-1 md:py-2 xl:py-4"
+        >
+          <p
+            class="tracking-wider text-t3 md:text-h5 lg:text-h4 q-px-sm whitespace-nowrap font-semibold"
+          >
+            Скачать
+          </p>
+        </q-btn>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -102,6 +168,31 @@
 }
 .help-section {
   background-image: url("/images/index-page/help-bg.png");
+}
+
+.load-app-section {
+  background-image: url("/images/index-page/load-app-bg.png");
+}
+
+@media (min-width: 1440px) {
+  .hosts-section {
+    background-image: url("/images/index-page/hosts-bg-xl.png");
+  }
+}
+@media (min-width: 1280px) and (max-width: 1440px) {
+  .hosts-section {
+    background-image: url("/images/index-page/hosts-bg-lg.png");
+  }
+}
+@media (min-width: 768px) and (max-width: 1280px) {
+  .hosts-section {
+    background-image: url("/images/index-page/hosts-bg-md.png");
+  }
+}
+@media (max-width: 768px) {
+  .hosts-section {
+    background-image: url("/images/index-page/hosts-bg-sm.png");
+  }
 }
 </style>
 <script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     container: {},
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1280px",
+      xl: "1440px",
+      "2xl": "1920px",
+    },
     extend: {
       colors: {
         "rent-red": "#FF493E",
@@ -15,6 +22,14 @@ module.exports = {
         "rent-grey": "#DBDBDB",
         "rent-light-grey": "#E8EFF4",
       },
+      height: (theme) => ({
+        "screen*90": "90vh",
+        "screen*80": "80vh",
+        "screen/2": "50vh",
+        "screen/3": "calc(100vh / 3)",
+        "screen/4": "calc(100vh / 4)",
+        "screen/5": "calc(100vh / 5)",
+      }),
     },
   },
   plugins: [],
