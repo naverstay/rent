7dd7072093c91fc86fc8a846ef24338086aadac4
mavrin <mavrin_88@mail.ru>
2023-02-20 17:30:54 +0300

    Add icon component

---
+++ src/components/Icons/BaseIcon.vue
@@ -0,0 +1,37 @@
+<template>
+  <component
+    :is="iconComponent"
+    role="img"
+    class="inline-block fill-current"
+  />
+</template>
+
+<script>
+import kebabCase from 'lodash/kebabCase'
+
+const icons = {}
+const requireComponents = require.context('./', false, /[\w]+Icon\.vue$/)
+
+requireComponents.keys().forEach(fileName => {
+  const iconName = kebabCase(fileName.replace(/^\.\/(.+)Icon\.vue/, '$1'))
+  const componentConfig = requireComponents(fileName)
+  icons[iconName] = componentConfig.default || componentConfig
+})
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+      validator(value) {
+        return Object.prototype.hasOwnProperty.call(icons, value)
+      }
+    }
+  },
+
+  computed: {
+    iconComponent() {
+      return icons[this.name]
+    }
+  }
+}
+</script>
